d96ddce28ed7ec5a89061656c06cf54a9807908f
shrexB <shrexchauhan@gmail.com>
2016-09-12 20:28:59 -0400

    commit before merging.

--- hello.java
+++ hello.java
@@ -5,7 +5,7 @@ main(){
 String fname = "shreyas";
 String lname = "Chauhan";
 System.out.println("Hello Git!! " + fname + lname + "cheap thrills !!!");
-System.out.println("tudu du du du!!");
+System.out.println("tudu du du du!! zzzz");
 }
 
 }
